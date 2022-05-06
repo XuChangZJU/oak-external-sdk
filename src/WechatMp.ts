c3399f36b09371541d5908286cb849c6ab5a0ca2
Xc <chinawraith@163.com>
2022-05-06 19:33:03 +0800

    解压

--- src/WechatMp.ts
+++ src/WechatMp.ts
@@ -1,3 +1,6 @@
+import crypto from 'crypto';
+import  { Buffer } from 'buffer';
+
 export class WechatMpInstance {
     appId: string;
     appSecret: string;
@@ -17,5 +20,26 @@ export class WechatMpInstance {
             openId: openid as string,
             unionId: unionid as string,
         };
+    }
+
+    decryptData(sessionKey: string, encryptedData: string, iv: string, signature: string) {
+        const skBuf = Buffer.from(sessionKey, 'base64');
+        // const edBuf = Buffer.from(encryptedData, 'base64');
+        const ivBuf = Buffer.from(iv, 'base64');
+
+        
+        const decipher = crypto.createDecipheriv('aes-128-cbc', skBuf, ivBuf);
+        // 设置自动 padding 为 true，删除填充补位
+        decipher.setAutoPadding(true);
+        let decoded = decipher.update(encryptedData, 'base64', 'utf8');
+        decoded += decipher.final('utf8');
+    
+        const data = JSON.parse(decoded);
+
+        if (data.watermark.appid !== this.appId) {
+            throw new Error('Illegal Buffer');
+        }
+
+        return data;
     }
 };
