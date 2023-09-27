355ca0084f1c659b1448e3e7ba2b816c476a6949
wkj <278599135@qq.com>
2023-09-27 15:51:54 +0800

    build

--- es/service/amap/Amap.js
+++ es/service/amap/Amap.js
@@ -1,5 +1,5 @@
 require('../../fetch');
-import { OakExternalException, } from 'oak-domain/lib/types/Exception';
+import { OakExternalException, OakNetworkException, } from 'oak-domain/lib/types/Exception';
 export class AmapInstance {
     key;
     constructor(key) {
@@ -8,8 +8,14 @@ export class AmapInstance {
     async getDrivingPath(data) {
         const { from, to } = data;
         const url = `http://restapi.amap.com/v3/direction/driving?origin=${from[0].toFixed(6)},${from[1].toFixed(6)}&destination=${to[0].toFixed(6)},${to[1].toFixed(6)}&strategy=10&key=${this.key}`;
-        const result = await global.fetch(url);
-        const jsonData = await result.json();
+        let response;
+        try {
+            response = await global.fetch(url);
+        }
+        catch (err) {
+            throw new OakNetworkException(`访问amap接口失败，「${url}」`);
+        }
+        const jsonData = await response.json();
         if (jsonData.status !== '1') {
             throw new OakExternalException('amap', jsonData.infocode, jsonData.info);
         }
@@ -17,8 +23,15 @@ export class AmapInstance {
     }
     async regeo(data) {
         const { longitude, latitude } = data;
-        const result = await global.fetch(`https://restapi.amap.com/v3/geocode/regeo?location=${longitude},${latitude}&key=${this.key}`);
-        const jsonData = await result.json();
+        const url = `https://restapi.amap.com/v3/geocode/regeo?location=${longitude},${latitude}&key=${this.key}`;
+        let response;
+        try {
+            response = await global.fetch(url);
+        }
+        catch (err) {
+            throw new OakNetworkException(`访问amap接口失败，「${url}」`);
+        }
+        const jsonData = await response.json();
         if (jsonData.status !== '1') {
             throw new OakExternalException('amap', jsonData.infocode, jsonData.info);
         }
@@ -27,8 +40,14 @@ export class AmapInstance {
     async ipLoc(data) {
         const { ip } = data;
         const url = `https://restapi.amap.com/v3/ip?key=${this.key}&ip=${ip}`;
-        const result = await global.fetch(url);
-        const jsonData = await result.json();
+        let response;
+        try {
+            response = await global.fetch(url);
+        }
+        catch (err) {
+            throw new OakNetworkException(`访问amap接口失败，「${url}」`);
+        }
+        const jsonData = await response.json();
         if (jsonData.status !== '1') {
             throw new OakExternalException('amap', jsonData.infocode, jsonData.info);
         }
@@ -37,8 +56,14 @@ export class AmapInstance {
     async getDistrict(data) {
         const { keywords, subdistrict } = data;
         const url = `https://restapi.amap.com/v3/config/district?keywords=${keywords}&subdistrict=${subdistrict}&key=${this.key}`;
-        const result = await global.fetch(url);
-        const jsonData = await result.json();
+        let response;
+        try {
+            response = await global.fetch(url);
+        }
+        catch (err) {
+            throw new OakNetworkException(`访问amap接口失败，「${url}」`);
+        }
+        const jsonData = await response.json();
         if (jsonData.status !== '1') {
             throw new OakExternalException('amap', jsonData.infocode, jsonData.info);
         }
@@ -47,8 +72,14 @@ export class AmapInstance {
     async geocode(data) {
         const { address } = data;
         const url = `https://restapi.amap.com/v3/geocode/geo?address=${address}&key=${this.key}`;
-        const result = await global.fetch(url);
-        const jsonData = await result.json();
+        let response;
+        try {
+            response = await global.fetch(url);
+        }
+        catch (err) {
+            throw new OakNetworkException(`访问amap接口失败，「${url}」`);
+        }
+        const jsonData = await response.json();
         if (jsonData.status !== '1') {
             throw new OakExternalException('amap', jsonData.infocode, jsonData.info);
         }
