8bd2d10171f2277a7e67697646433bb67b901f8a
Xc <chinawraith@163.com>
2023-11-27 13:09:01 +0800

    native适配

--- es/service/tencent/Sms.web.d.ts
+++ es/service/tencent/Sms.web.d.ts
@@ -1,10 +1,4 @@
 import { SendSmsRequest, SendSmsResponse } from 'tencentcloud-sdk-nodejs/tencentcloud/services/sms/v20210111/sms_models';
 export declare class TencentSmsInstance {
-    secretId: string;
-    secretKey: string;
-    region: string;
-    endpoint: string;
-    client: any;
-    constructor(secretId: string, secretKey: string, region: string, endpoint: string);
     sendSms(params: SendSmsRequest): Promise<SendSmsResponse>;
 }
