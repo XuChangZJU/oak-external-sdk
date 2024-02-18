4b2566431e05f91e9cf84eee6e4b87b0c20ef294
wenjiarui <wenjiarui1@126.com>
2024-02-18 14:15:26 +0800

    index先放开

--- es/index.js
+++ es/index.js
@@ -1,27 +1,8 @@
-/* import WechatSDK, { WechatMpInstance, WechatPublicInstance, WechatWebInstance } from './WechatSDK';
+import WechatSDK, { WechatMpInstance, WechatPublicInstance, WechatWebInstance } from './WechatSDK';
 import AmapSDK from './AmapSDK';
 import QiniuSDK, { QiniuCloudInstance } from './QiniuSDK';
-import SmsSdk, {
-    TencentSmsInstance,
-    AliSmsInstance,
-    CTYunSmsInstance,
-} from './SmsSdk';
+import SmsSdk, { TencentSmsInstance, AliSmsInstance, CTYunSmsInstance, } from './SmsSdk';
 import CTYunSDk, { CTYunInstance } from './CTYunSDK';
-
 export * from './service/amap/Amap';
-export {
-    AmapSDK,
-    QiniuSDK,
-    WechatSDK,
-    CTYunSDk,
-    CTYunInstance,
-    WechatMpInstance,
-    WechatPublicInstance,
-    WechatWebInstance,
-    QiniuCloudInstance,
-    SmsSdk,
-    TencentSmsInstance,
-    AliSmsInstance,
-    CTYunSmsInstance,
-};*/
+export { AmapSDK, QiniuSDK, WechatSDK, CTYunSDk, CTYunInstance, WechatMpInstance, WechatPublicInstance, WechatWebInstance, QiniuCloudInstance, SmsSdk, TencentSmsInstance, AliSmsInstance, CTYunSmsInstance, };
 export * from './types';
